4801286ce3bb2f3b909d4c31a9dc37824bd54096
godieGH <lilwizzemcee@gmail.com>
2025-05-17 14:05:28 +0300

    At this stage my app can do proper logins and account creation, it is an spa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <router-view />
+   <q-layout>
+      <router-view />
+   </q-layout>
 </template>
 
 <script setup>
-//
+   import {
+      onMounted
+   } from "vue"
+   import {
+      useRouter
+   } from 'vue-router'
+   import axios from "axios"
+   axios.defaults.withCredentials = true
+   const baseUrl = import.meta.env.VITE_API_BASE_URL
+
+
+   const router = useRouter()
+
+
+   onMounted(async () => {
+      try {
+         console.log(baseUrl)
+         const res = await axios.post(`${baseUrl}/users/token/refresh`)
+         axios.defaults.headers.common['Authorization'] = `Bearer ${res.data.accessToken}`;
+         //redirect user to access the pages and data
+         
+         console.log(res.data)
+      } catch(err) {
+         if (err) {
+            router.push('/login')
+         }
+      }
+   })
+
+
+
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,29 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/AuthLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: '/login',
+        component: () => import('pages/LoginPage.vue'),
+      },
+      {
+        path: '/register',
+        component: () => import('pages/RegisterPage.vue'),
+      },
+      {
+        path: '/forgot-password',
+        component: () => import('pages/ForgotPasswordPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
